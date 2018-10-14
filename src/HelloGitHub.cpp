e10e249304385edd8a4fd99d0f152e88b7d4466f
Gustavo <gustavo.marino.presente@gmail.com>
2018-10-14 11:08:23 -0300

    segundo commite

--- src/HelloGitHub.cpp
+++ src/HelloGitHub.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	cout << "Hello World Testando GitHub!!!" << endl; // prints Hello World!!!
 	return 0;
 }
